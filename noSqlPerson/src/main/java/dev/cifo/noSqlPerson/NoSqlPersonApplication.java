940e8da1a2ca11e67c692bb4d1c8c921267b1ca2
albertprofe <albertprofe@gmail.com>
2026-03-30 09:41:56 +0200

    first commit

---
+++ noSqlPerson/src/main/java/dev/cifo/noSqlPerson/NoSqlPersonApplication.java
@@ -0,0 +1,13 @@
+package dev.cifo.noSqlPerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSqlPersonApplication.class, args);
+	}
+
+}
---
+++ noSqlPerson/src/test/java/dev/cifo/noSqlPerson/NoSqlPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cifo.noSqlPerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoSqlPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
